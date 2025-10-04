11c07f53be5a46b714e6762bdf82e35499cd5163
fullstackmedusa <alo.maldonado96@gmail.com>
2025-10-03 17:57:52 -0700

    CRUD complete

--- controllers/applications.js
+++ controllers/applications.js
@@ -3,9 +3,12 @@ const router = express.Router()
 const User = require('../models/user')
 
 ///users/:userId/applications
-router.get('/', (req,res) => {
+router.get('/', async (req,res) => {
     try{
-        res.render('applications/index.ejs')
+        const currentUser = await User.findById(req.session.user._id)
+        res.render('applications/index.ejs',{
+            applications: currentUser.applications
+        })
     } catch (error){
         console.log(error)
         res.redirect('/')
@@ -28,5 +31,57 @@ router.post('/', async (req, res) =>{
     }
 })
 
+// GET /users/:userId/applications/:applicationId
+router.get('/:applicationId', async (req, res) => {
+    try{
+        const currentUser = await User.findById(req.session.user._id)
+        const application = currentUser.applications.id(req.params.applicationId)
+        res.render('applications/show.ejs', {application:application})
+    } catch (error){
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
+// DELETE /users/:userId/applications/:applicationId
+router.delete('/:applicationId', async (req, res)=>{
+    try{
+        const currentUser = await User.findById(req.session.user._id)
+        currentUser.applications.id(req.params.applicationId).deleteOne()
+        await currentUser.save()
+        res.redirect(`/users/${currentUser._id}/applications`)
+    } catch(error){
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
+// GET /users/:userId/applications/:applicationId/edit
+router.get('/:applicationId/edit', async (req,res)=>{
+    try{
+        const currentUser = await User.findById(req.session.user._id)
+        const application = currentUser.applications.id(req.params.applicationId)
+        res.render('applications/edit.ejs',{
+            application: application
+        })
+    } catch(error){
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
+// PUT /users/:userId/applications/:applicationId
+router.put('/:applicationId', async (req,res)=>{
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        const application = currentUser.applications.id(req.params.applicationId)
+        application.set(req.body)
+        await currentUser.save()
+        res.redirect(`/users/${currentUser._id}/applications/${req.params.applicationId}`)
+    } catch(error){
+        console.log(error)
+        res.redirect('/')
+    }
+})
 
 module.exports = router
